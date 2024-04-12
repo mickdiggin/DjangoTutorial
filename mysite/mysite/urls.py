a2b8a67cbf9d0f2dd104913a0e124225a395fdbc
mickdiggin <m_degan@mail.fhsu.edu>
2024-04-12 18:48:27 -0500

    First working version of homepage app.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -22,6 +22,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('homepage.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
